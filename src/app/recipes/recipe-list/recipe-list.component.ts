fadc5eac6c6811abfd92a354900cdcbb8815d75d
Rogerio Teixeira <rogerio.teixeira@hexagon.com>
2021-10-12 16:49:11 +0200

    Added selection of recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,11 +8,28 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
 
-
+  public recipes: Recipe[] = [
+    {
+      name: "Recipe 1",
+      description: "Recipe 1 Desc",
+      imagePath: "https://i.pinimg.com/736x/4e/d3/36/4ed33645f07fa2ec1de7a56d9ff918a8.jpg"
+    },
+    {
+      name: "Recipe 2",
+      description: "Recipe 2 Desc",
+      imagePath: "https://th.bing.com/th/id/OIP.-5eWeRXcvrNXm4Jl8JSOZQHaIe?pid=ImgDet&rs=1"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  public selectRecipe(recipe: Recipe): void {
+    this.recipeSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent {
+
+  public recipe!: Recipe;
 
   constructor() { }
 
-  ngOnInit(): void {
+  public setRecipe(recipe: Recipe) {
+    this.recipe = recipe;
   }
 
 }
