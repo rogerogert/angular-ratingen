5ce54206ab4dee8337caa65a1ab0bc700b7bc23d
Rogerio Teixeira <rogerio.teixeira@hexagon.com>
2021-10-12 13:42:48 +0200

    input component

--- src/app/test/test-child/test-child.component.ts
+++ src/app/test/test-child/test-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-child',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-child.component.css']
 })
 export class TestChildComponent implements OnInit {
+
+  @Input() public fullName = '';
 
   constructor() { }
 
