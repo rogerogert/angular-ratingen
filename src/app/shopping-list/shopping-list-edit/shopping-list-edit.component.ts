b757746385d9689aad676de95e56d83db0b8306f
Rogerio Teixeira <rogerio.teixeira@hexagon.com>
2021-10-14 16:45:12 +0200

    cehfini

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('slf') public slForm!: NgForm
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+  }
+
+  public onSubmit(): void {
+    console.log(this.slForm);
+    let ingredient: Ingredient = {
+      name: this.slForm.value.name,
+      amount: this.slForm.value.amount
+    }
+    this.shoppingListService.addIngredient(ingredient);
+    this.reset();
+  }
+
+  public reset(): void {
+    this.slForm.reset();
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-  public ingredients: Ingredient[] = [
-    new Ingredient('Tomato', 1),
-    new Ingredient('Beans', 5)
-  ];
-  constructor() { }
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  public ingredients!: Ingredient[];
+  private ingredientsSubs!: Subscription;
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.shoppingListService.getIngredients();
+
+    this.ingredientsSubs = this.shoppingListService.ingredientsAdded.subscribe(ingredients => {
+      this.ingredients = ingredients;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.ingredientsSubs.unsubscribe();
   }
 
 }
